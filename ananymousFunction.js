afa9d8ce070eaa4d7f7bec24e838af0a4a1a26ac
Mahesh <itstribe.mahi@gmail.com>
2022-08-30 16:32:15 +0530

    Using ananymous Function

---
+++ ananymousFunction.js
@@ -0,0 +1,9 @@
+//ananymous function enclosed with ().
+(function(value){
+  // let value="CGCS"
+   arr=[];
+  for(i of value ){
+    console.log(i);
+
+  }
+})('mahesh');// ananymous function should call with ()
